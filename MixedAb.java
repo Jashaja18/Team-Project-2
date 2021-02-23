dbbbb70973362c43a788eff6b3aebb75950d94a9
Jashaja18 <67085053+Jashaja18@users.noreply.github.com>
2021-02-22 18:16:20 -0800

    Update MixedAb.java
    This is what I found so far. Please feel free to add more I will also do that as well.

--- MixedAb.java
+++ MixedAb.java
@@ -1,1 +1,28 @@
+// This is the Abstract class with the mixed duplicates from the ArrayList and LinkedList
+
+public class MixedAb {
+
+	// post: returns the current number of elements in the list
+	public int size() {
+		return size;
+	}
+
+	// pre : 0 <= index < size() (throws IndexOutOfBoundsException if not)
+	// post: returns the value at the given index in the list
+	public E get(int index) {
+		checkIndex(index);
+		return elementData[index];
+	}
+
+	// post: returns true if list is empty, false otherwise
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	// post: returns true if the given value is contained in the list,
+	//       false otherwise
+	public boolean contains(E value) {
+		return indexOf(value) >= 0;
+	}
+}
 
