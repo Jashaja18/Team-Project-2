0ed53c995c9ce2c56d2b83d22068523be17d8e53
Jashaja18 <67085053+Jashaja18@users.noreply.github.com>
2021-02-25 18:45:04 -0800

    Update AbstractList.java

--- AbstractList.java
+++ AbstractList.java
@@ -10,8 +10,7 @@ import java.util.*;
 
 public abstract class AbstractList<E> implements List<E>
 {
-    private int size;        // current number of elements in the list
-    private E[] elementData; // list of values
+    public int size; // current number of elements in the list
 
     // post: returns the current number of elements in the list
     public int size()
@@ -42,10 +41,20 @@ public abstract class AbstractList<E> implements List<E>
         }
     }
     
+    // post: returns true if the given value is contained in the list,
+    //       false otherwise
+    public boolean contains(E value) 
+    {
+        return indexOf(value) >= 0;
+    }
+    
     // post: list is empty
-    public void clear() {
+    // Jashanpreet Jandu
+    public void clear() 
+    {
         Iterator<E> itrE = this.iterator();
-        while(itrE.hasNext()) {
+        while(itrE.hasNext()) 
+        {
                 itrE.next():
                 itrE.remove();
         }
