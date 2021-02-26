5dcd0d4ce7a65633bf673f17aa3a1ac4c8c77f20
Jashaja18 <67085053+Jashaja18@users.noreply.github.com>
2021-02-25 18:47:29 -0800

    Update AbstractList.java

--- AbstractList.java
+++ AbstractList.java
@@ -18,14 +18,6 @@ public abstract class AbstractList<E> implements List<E>
         return size;
     }
 
-    // pre : 0 <= index < size() (throws IndexOutOfBoundsException if not)
-    // post: returns the value at the given index in the list
-    public E get(int index)
-    {
-        checkIndex(index);
-        return elementData[index];
-    }
-
     // post: returns true if list is empty, false otherwise
     public boolean isEmpty()
     {
