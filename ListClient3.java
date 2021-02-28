d272f45f9d18dd8ba375f584498e1bc2090ce544
Jashaja18 <67085053+Jashaja18@users.noreply.github.com>
2021-02-28 13:28:07 -0800

    Update ListClient3.java

--- ListClient3.java
+++ ListClient3.java
@@ -3,22 +3,5 @@
 
 public class ListClient3 {
     public static void main(String[] args) {
-        ArrayList<Integer> list1 = new ArrayList<Integer>();
-        processList(list1);
-        System.out.println();
-
-        LinkedList<Integer> list2 = new LinkedList<Integer>();
-        processList(list2);
-    }
-
-    public static void processList(List list) {
-        list.add(42);
-        list.add(18);
-        list.add(27);
-        list.add(93);
-        System.out.println(list);
-        list.remove(2);
-        list.remove(0);
-        System.out.println(list);
-    }
+       
 }
