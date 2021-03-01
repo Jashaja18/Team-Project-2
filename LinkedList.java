4a1a3b4aae313c39cddbc000923d810bebba1eb9
Jashanpreet Jandu <67085053+Jashaja18@users.noreply.github.com>
2021-02-28 22:16:11 -0800

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,4 +1,3 @@
-
 /*
  * Group 4
  * Mar 1 2021
@@ -16,36 +15,46 @@ public class LinkedList<E> extends AbstractList<E>
 	// private int size;           // current number of elements
 
 	// post: constructs an empty list
-	public LinkedList() {
+	public LinkedList() 
+	{
 		front = new ListNode<E>(null);
 		back = new ListNode<E>(null);
 		clear();
 	}
 
 	/* post: returns the current number of elements in the list
-	public int size() {
+	public int size() 
+	{
 		return size;
 	}*/
 
 	/* pre : 0 <= index < size() (throws IndexOutOfBoundsException if not)
 	// post: returns the value at the given index in the list
-	public E get(int index) {
+	public E get(int index) 
+	{
 		checkIndex(index);
 		ListNode<E> current = nodeAt(index);
 		return current.data;
 	}*/
 
 	/* post: creates a comma-separated, bracketed version of the list
-	public String toString() {
-		if (size == 0) {
+	public String toString() 
+	{
+		if (size == 0) 
+		{
 			return "[]";
-		} else {
+		} 
+		
+		else 
+		{
 			String result = "[" + front.next.data;
 			ListNode<E> current = front.next.next;
-			while (current != back) {
+			while (current != back) 
+			{
 				result += ", " + current.data;
 				current = current.next;
 			}
+			
 			result += "]";
 			return result;
 		}
@@ -53,11 +62,14 @@ public class LinkedList<E> extends AbstractList<E>
 
 	/* post : returns the position of the first occurrence of the given
 	//        value (-1 if not found)
-	public int indexOf(E value) {
+	public int indexOf(E value) 
+	{
 		int index = 0;
 		ListNode<E> current = front.next;
-		while (current !=  back) {
-			if (current.data.equals(value)) {
+		while (current !=  back)
+		 {
+			if (current.data.equals(value)) 
+			{
 				return index;
 			}
 			index++;
@@ -67,28 +79,34 @@ public class LinkedList<E> extends AbstractList<E>
 	}*/
 
 	/* post: returns true if list is empty, false otherwise
-	public boolean isEmpty() {
+	public boolean isEmpty()
+	 {
 		return size == 0;
 	}*/
 
 	/* post: returns true if the given value is contained in the list,
 	//       false otherwise
-	public boolean contains(E value) {
+	public boolean contains(E value) 
+	{
 		return indexOf(value) >= 0;
 	}*/
 
 	// post: appends the given value to the end of the list
-	public void add(E value) {
+	public void add(E value) 
+	{
 		add(size, value);
 	}
 
 	// pre: 0 <= index <= size() (throws IndexOutOfBoundsException if not)
 	// post: inserts the given value at the given index, shifting subsequent
 	//       values right
-	public void add(int index, E value) {
-		if (index < 0 || index > size) {
+	public void add(int index, E value)
+	{
+		if (index < 0 || index > size) 
+		{
 			throw new IndexOutOfBoundsException("index: " + index);
 		}
+		
 		ListNode<E> current = nodeAt(index - 1);
 		ListNode<E> newNode = new ListNode<E>(value, current.next, current);
 		current.next = newNode;
@@ -97,15 +115,18 @@ public class LinkedList<E> extends AbstractList<E>
 	}
 
 	/* post: appends all values in the given list to the end of this list
-	public void addAll(List<E> other) {
-		for (E value: other) {
+	public void addAll(List<E> other) 
+	{
+		for (E value: other) 
+		{
 			add(value);
 		}
 	}*/
 
 	// pre : 0 <= index < size() (throws IndexOutOfBoundsException if not)
 	// post: removes value at the given index, shifting subsequent values left
-	public void remove(int index) {
+	public void remove(int index)
+	{
 		checkIndex(index);
 		ListNode<E> current = nodeAt(index - 1);
 		current.next = current.next.next;
@@ -115,21 +136,24 @@ public class LinkedList<E> extends AbstractList<E>
 
 	/* pre : 0 <= index < size() (throws IndexOutOfBoundsException if not)
 	// post: replaces the value at the given index with the given value
-	public void set(int index, E value) {
+	public void set(int index, E value) 
+	{
 		checkIndex(index);
 		ListNode<E> current = nodeAt(index);
 		current.data = value;
 	}*/
 
 	// post: list is empty
-	public void clear() {
+	public void clear()
+	{
 		front.next = back;
 		back.prev = front;
 		size = 0;
 	}
 
 	// post: returns an iterator for this list
-	public Iterator<E> iterator() {
+	public Iterator<E> iterator()
+	{
 		return new LinkedIterator();
 	}
 
@@ -137,16 +161,24 @@ public class LinkedList<E> extends AbstractList<E>
 	// post: returns the node at a specific index.  Uses the fact that the list
 	//       is doubly-linked to start from the front or the back, whichever
 	//       is closer.
-	private ListNode<E> nodeAt(int index) {
+	private ListNode<E> nodeAt(int index) 
+	{
 		ListNode<E> current;
-		if (index < size / 2) {
+		if (index < size / 2) 
+		{
 			current = front;
-			for (int i = 0; i < index + 1; i++) {
+			for (int i = 0; i < index + 1; i++)
+			{
 				current = current.next;
 			}
-		} else {
+			
+		} 
+		
+		else 
+		{
 			current = back;
-			for (int i = size; i >= index + 1; i--) {
+			for (int i = size; i >= index + 1; i--) 
+			{
 				current = current.prev;
 			}
 		}
@@ -155,51 +187,62 @@ public class LinkedList<E> extends AbstractList<E>
 
 	/* post: throws an IndexOutOfBoundsException if the given index is
 	//       not a legal index of the current list
-	private void checkIndex(int index) {
-		if (index < 0 || index >= size()) {
+	private void checkIndex(int index) 
+	{
+		if (index < 0 || index >= size())
+		{
 			throw new IndexOutOfBoundsException("index: " + index);
 		}
 	}*/
 
-	private static class ListNode<E> {
+	private static class ListNode<E>
+	{
 		public E data;         // data stored in this node
 		public ListNode<E> next;  // link to next node in the list
 		public ListNode<E> prev;  // link to previous node in the list
 
 		// post: constructs a node with given data and given links
-		public ListNode(E data, ListNode<E> next, ListNode<E> prev) {
+		public ListNode(E data, ListNode<E> next, ListNode<E> prev)
+		{
 			this.data = data;
 			this.next = next;
 			this.prev = prev;
 		}
 
 		// post: constructs a node with given data and null links
-		public ListNode(E data) {
+		public ListNode(E data) 
+		{
 			this(data, null, null);
 		}
 	}
 
-	private class LinkedIterator implements Iterator<E> {
+	private class LinkedIterator implements Iterator<E> 
+	{
 		private ListNode<E> current;  // location of next value to return
 		private boolean removeOK;  // whether it's okay to remove now
 
 		// post: constructs an iterator for the given list
-		public LinkedIterator() {
+		public LinkedIterator() 
+		{
 			current = front.next;
 			removeOK = false;
 		}
 
 		// post: returns true if there are more elements left, false otherwise
-		public boolean hasNext() {
+		public boolean hasNext()
+		{
 			return current != back;
 		}
 
 		// pre : hasNext()
 		// post: returns the next element in the iteration
-		public E next() {
-			if (!hasNext()) {
+		public E next()
+		{
+			if (!hasNext()) 
+			{
 				throw new NoSuchElementException();
 			}
+			
 			E result = current.data;
 			current = current.next;
 			removeOK = true;
@@ -209,10 +252,13 @@ public class LinkedList<E> extends AbstractList<E>
 		// pre : next() has been called without a call on remove (i.e., at most
 		//       one call per call on next)
 		// post: removes the last element returned by the iterator
-		public void remove() {
-			if (!removeOK) {
+		public void remove()
+		{
+			if (!removeOK)
+			{
 				throw new IllegalStateException();
 			}
+			
 			ListNode<E> prev2 = current.prev.prev;
 			prev2.next = current;
 			current.prev = prev2;
