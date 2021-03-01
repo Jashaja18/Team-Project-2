15cea3629e6b14bea6c448f5c09e936e616cbc0e
tyribeiro <77495513+tyribeiro@users.noreply.github.com>
2021-02-28 22:13:08 -0500

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -1,202 +1,73 @@
-
 /*
  * Group 4
  * Mar 1 2021
  * CS 211
  * Team Project 2
- * Class ArrayList<E> can be used to store a list of values of type E
+ * Client class to test project
  */
 
-import java.util.*;
+public class ListClient {
+    public static void main(String[] args)
+    {
 
-public class ArrayList<E> extends AbstractList<E> 
-{
-	private E[] elementData; // list of values
-	// private int size;        // current number of elements in the list
+        // test arraylist for addAll()
 
-	public static final int DEFAULT_CAPACITY = 100;
 
-	// pre : capacity >= 0 (throws IllegalArgumentException if not)
-	// post: constructs an empty list with the given capacity
-	@SuppressWarnings("unchecked")
-	public ArrayList(int capacity) {
-		if (capacity < 0) {
-			throw new IllegalArgumentException("capacity: " + capacity);
-		}
-		elementData = (E[]) new Object[capacity];
-		size = 0;
-	}
+        // ArrayList testing
+        ArrayList<Integer> testArray = new ArrayList<Integer>();
+        // add random numbers
+        testArray.add(5);
+        testArray.add(76);
+        testArray.add(-32);
+        testArray.add(0);
+        testArray.add(3244523);
+        testArray.add(342);
 
-	// post: constructs an empty list of default capacity
-	public ArrayList() {
-		this(DEFAULT_CAPACITY);
-	}
+        System.out.println("---- Arraylist ----");
+        testList(testArray);
 
-	/* post: returns the current number of elements in the list
-	public int size() {
-		return size;
-	}*/
+        // LinkedList testing
+        LinkedList<Integer> testList = new LinkedList<Integer>();
 
-	// pre : 0 <= index < size() (throws IndexOutOfBoundsException if not)
-	// post: returns the value at the given index in the list
-	public E get(int index) {
-		checkIndex(index);
-		return elementData[index];
-	}
+        // add random numbers
+        testList.add(532);
+        testList.add(8);
+        testList.add(645);
+        testList.add(-65);
+        testList.add(3);
+        testList.add(0);
 
-	/* post: creates a comma-separated, bracketed version of the list
-	public String toString() {
-		if (size == 0) {
-			return "[]";
-		} else {
-			String result = "[" + elementData[0];
-			for (int i = 1; i < size; i++) {
-				result += ", " + elementData[i];
-			}
-			result += "]";
-			return result;
-		}
-	}*/
+        System.out.println("---- LinkedList ----");
+        testList(testList);
+    }
 
-	/* post : returns the position of the first occurrence of the given
-	//        value (-1 if not found)
-	public int indexOf(E value) {
-		for (int i = 0; i < size; i++) {
-			if (elementData[i].equals(value)) {
-				return i;
-			}
-		}
-		return -1;
-	}*/
+    // tests functionality of shared methods
+    public static void testList(AbstractList<Integer> test) {
 
-	/* post: returns true if list is empty, false otherwise
-	public boolean isEmpty() {
-		return size == 0;
-	}*/
+        System.out.println("Test List (Initial State): " + test);
+        System.out.println("size() test: " + test.size());
+        System.out.println("isEmpty test: " + test.isEmpty());
+        System.out.println("get(4) test: " + test.get(4));
 
-	/* post: returns true if the given value is contained in the list,
-	//       false otherwise
-	public boolean contains(E value) {
-		return indexOf(value) >= 0;
-	}*/
 
-	// post: appends the given value to the end of the list
-	public void add(E value) {
-		ensureCapacity(size + 1);
-		elementData[size] = value;
-		size++;
-	}
+        test.remove(4);
+        System.out.println("Test List (after remove(4): " + test);
 
-	// pre : 0 <= index <= size() (throws IndexOutOfBoundsException if not)
-	// post: inserts the given value at the given index, shifting subsequent
-	//       values right
-	public void add(int index, E value) {
-		if (index < 0 || index > size) {
-			throw new IndexOutOfBoundsException("index: " + index);
-		}
-		ensureCapacity(size + 1);
-		for (int i = size; i >= index + 1; i--) {
-			elementData[i] = elementData[i - 1];
-		}
-		elementData[index] = value;
-		size++;
-	}
+        test.add(3, 69);
+        System.out.println("Test List (after add(3, 69): " + test);
 
-	// pre : 0 <= index < size() (throws IndexOutOfBoundsException if not)
-	// post: removes value at the given index, shifting subsequent values left
-	public void remove(int index) {
-		checkIndex(index);
-		for (int i = index; i < size - 1; i++) {
-			elementData[i] = elementData[i + 1];
-		}
-		elementData[size - 1] = null;
-		size--;
-	}
+        test.clear();
+        System.out.println("Test List (after clear()): " + test);
 
-	/* pre : 0 <= index < size() (throws IndexOutOfBoundsException if not)
-	// post: replaces the value at the given index with the given value
-	public void set(int index, E value) {
-		checkIndex(index);
-		elementData[index] = value;
-	}*/
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        temp.add(1);
+        temp.add(2);
+        temp.add(3);
+        test.addAll(temp);
+        System.out.println("temp: " + temp);
+        System.out.println("Test List (after addAll(temp)): " + test);
 
-	/* post: list is empty
-	public void clear() {
-		for (int i = 0; i < size; i++) {
-			elementData[i] = null;
-		}
-		size = 0;
-	}*/
+        System.out.println();
+    }
 
-	/* post: appends all values in the given list to the end of this list
-	public void addAll(List<E> other) {
-		for (E value: other) {
-			add(value);
-		}
-	}*/
-
-	// post: returns an iterator for this list
-	public Iterator<E> iterator() {
-		return new ArrayListIterator();
-	}
-
-	// post: ensures that the underlying array has the given capacity; if not,
-	//       the size is doubled (or more if given capacity is even larger)
-	public void ensureCapacity(int capacity) {
-		if (capacity > elementData.length) {
-			int newCapacity = elementData.length * 2 + 1;
-			if (capacity > newCapacity) {
-				newCapacity = capacity;
-			}
-			elementData = Arrays.copyOf(elementData, newCapacity);
-		}
-	}
-
-	/* post: throws an IndexOutOfBoundsException if the given index is
-	//       not a legal index of the current list
-	private void checkIndex(int index) {
-		if (index < 0 || index >= size) {
-			throw new IndexOutOfBoundsException("index: " + index);
-		}
-	}*/
-
-	private class ArrayListIterator implements Iterator<E> {
-		private int position;           // current position within the list
-		private boolean removeOK;       // whether it's okay to remove now
-
-		// post: constructs an iterator for the given list
-		public ArrayListIterator() {
-			position = 0;
-			removeOK = false;
-		}
-
-		// post: returns true if there are more elements left, false otherwise
-		public boolean hasNext() {
-			return position < size();
-		}
-
-		// pre : hasNext() (throws NoSuchElementException if not)
-		// post: returns the next element in the iteration
-		public E next() {
-			if (!hasNext()) {
-				throw new NoSuchElementException();
-			}
-			E result = elementData[position];
-			position++;
-			removeOK = true;
-			return result;
-		}
-
-		// pre : next() has been called without a call on remove (throws
-		//       IllegalStateException if not)
-		// post: removes the last element returned by the iterator
-		public void remove() {
-			if (!removeOK) {
-				throw new IllegalStateException();
-			}
-			ArrayList.this.remove(position - 1);
-			position--;
-			removeOK = false;
-		}
-	}
 }
